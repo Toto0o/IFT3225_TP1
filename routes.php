124819e8058f1e7447dbca632198c2a493151acf
Antoine Tessier <antoine.tessier@umontreal.ca>
2026-02-21 14:37:59 -0500

    Ajout des routes pour users

--- routes.php
+++ routes.php
@@ -6,10 +6,16 @@ require_once __DIR__.'/router.php';
 // ##################################################
 // ##################################################
 
-//GET Tuiles
+//Tuiles routes
 get('/tuiles', 'api-rest/tuiles/read.php');
 put('/tuiles', 'api-rest/tuiles/update.php');
 delete('/tuiles', 'api-rest/tuiles/delete.php');
 post('/tuiles', 'api-rest/tuiles/create.php');
+
+//Users routes
+get('/users/$username', 'api-rest/users/read.php');
+put('/users', 'api-rest/users/update.php');
+delete('/users', 'api-rest/users/delete.php');
+post('/users', 'api-rest/users/create.php');
 
 any('/404', 'views/404.php');
